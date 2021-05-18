c6523d2ce16a651e554f646d684a7005efa5437e
Guillaume Doré cd guillaume.dore@protonmail.com <guillaume.dore@protonmail.com>
2021-05-18 14:17:22 +0200

    part 5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
